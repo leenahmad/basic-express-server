b7f1f21b363ae6c6fbb83a505063ece265f19b8d
leenahmad <leenkaraja24@gmail.com>
2022-02-23 15:59:31 +0200

    fix in validator test

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -5,20 +5,20 @@ module.exports = (req, res, next) => {
   const query = req.query;
   const name = query.name;
 
-  let regex = /^[a-zA-Z- ]+$/g
+  // let regex = /^[a-zA-Z- ]+$/g
 
-  if (regex.test(name)) {
-    next();
-  } else {
-    next(" error");
-  }
+  // if (regex.test(name)) {
+  //   next();
+  // } else {
+  //   next(" error");
+  // }
 
 
 
   
-//   if (name) {
-//     next();
-//   } else {
-//     next("500 / error");
-//   }
+  if (name) {
+    next();
+  } else {
+    next("500 / error");
+  }
 };
